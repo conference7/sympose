d75e0eef66819057268e5e74e86a80ef1d035f8b
Marinus Klasen <marinus@mklasen.nl>
2020-08-12 16:16:30 +0200

    Integrate git hooks

---
+++ scripts/setup-hooks.sh
@@ -0,0 +1,12 @@
+  #!/bin/sh
+  if [ ! -L .git/hooks ];
+  then
+      echo ".git/hooks is not symlink"
+      echo "copying .git/hooks to .git/old_hooks"
+      mv .git/hooks .git/old_hooks
+
+      echo "symlinking ../scripts/git-hooks .git/hooks"
+      ln -s ../scripts/git-hooks .git/hooks
+  else
+      echo ".git/hooks is already a symlink"
+  fi
