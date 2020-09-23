870b94dbecc9d842ce454b29d3e37093d77e73f2
Marinus Klasen <marinus@mklasen.nl>
2020-09-23 19:58:41 +0200

    First draft for personal agenda feature

--- js/dist/public/sympose.js
+++ js/dist/public/sympose.js
@@ -1,1 +1,83 @@
 "use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var schedules = document.querySelectorAll('.sympose-schedule.event');
+  var url = wpApiSettings.root + 'sympose/v1/update_agenda_sessions';
+  schedules.forEach(function (schedule) {
+    // Show stars
+    var savedSessions = localStorage.getItem('_sympose_saved_sessions');
+
+    if (savedSessions === null) {
+      savedSessions = {};
+    } else {
+      savedSessions = JSON.parse(savedSessions);
+    }
+
+    if (savedSessions[schedule.dataset.id] !== undefined) {
+      Object.keys(savedSessions[schedule.dataset.id]).map(function (key_id) {
+        var session_id = savedSessions[schedule.dataset.id][key_id];
+        var session = schedule.querySelector('.session-row[data-id="' + session_id + '"]');
+        var state = session.querySelector('.session-saved');
+        state.dataset.state = 'on';
+        session.classList.add('is-favorite');
+        schedule.dataset.starsHidden = false;
+      });
+    } // Save session
+
+
+    schedule.addEventListener('click', function (e) {
+      e.stopPropagation();
+
+      if (e.target.closest('.saved-sessions-control')) {
+        schedule.dataset.showFavorites = schedule.dataset.showFavorites === 'true' ? 'false' : 'true';
+      }
+
+      var target = e.target.closest('.session-saved');
+
+      if (target !== null) {
+        var eventID = target.closest('table').dataset.id;
+        var newState = target.dataset.state === 'off' ? 'on' : 'off';
+        var sessionID = target.parentNode.dataset.id;
+        target.dataset.state = newState; // Mark session as favorite
+
+        if (newState === 'on') {
+          target.parentNode.classList.add('is-favorite');
+        } else {
+          target.parentNode.classList.remove('is-favorite');
+        }
+
+        var headers = {
+          'X-WP-Nonce': wpApiSettings.nonce
+        };
+
+        var _savedSessions = localStorage.getItem('_sympose_saved_sessions');
+
+        if (_savedSessions === null) {
+          _savedSessions = [];
+        } else {
+          _savedSessions = JSON.parse(_savedSessions);
+        }
+
+        fetch(url, {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify({
+            'state': newState,
+            'id': sessionID,
+            'event_id': eventID,
+            'saved_sessions': _savedSessions
+          })
+        }).then(function (response) {
+          return response.json();
+        }).then(function (result) {
+          // Always update localStorage.
+          localStorage.setItem('_sympose_saved_sessions', JSON.stringify(result.data));
+
+          if (result.status === 200) {// that's fine.
+          }
+        });
+      }
+    });
+  });
+});
+"use strict";
---
+++ js/src/public/personal-agenda.js
@@ -0,0 +1,93 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	const schedules = document.querySelectorAll('.sympose-schedule.event');
+
+	const url = wpApiSettings.root + 'sympose/v1/update_agenda_sessions';
+
+	schedules.forEach((schedule) => {
+
+		// Show stars
+		let savedSessions = localStorage.getItem('_sympose_saved_sessions');
+
+		if (savedSessions === null) {
+			savedSessions = {};
+		} else {
+			savedSessions = JSON.parse(savedSessions);
+		}
+
+		if (savedSessions[schedule.dataset.id] !== undefined) {
+			Object.keys(savedSessions[schedule.dataset.id]).map((key_id) => {
+				let session_id = savedSessions[schedule.dataset.id][key_id];
+				let session = schedule.querySelector('.session-row[data-id="'+session_id+'"]');
+				let state = session.querySelector('.session-saved');
+				state.dataset.state = 'on';
+				session.classList.add('is-favorite');
+				schedule.dataset.starsHidden = false;
+			})
+		}
+
+		// Save session
+		schedule.addEventListener('click', (e) => {
+			e.stopPropagation();
+
+			if (e.target.closest('.saved-sessions-control')) {
+				schedule.dataset.showFavorites = (schedule.dataset.showFavorites === 'true' ? 'false' : 'true');
+			}
+
+			let target = e.target.closest('.session-saved');
+			if (target !== null) {
+				let eventID = target.closest('table').dataset.id;
+				let newState = (target.dataset.state === 'off' ? 'on' : 'off');
+				let sessionID = target.parentNode.dataset.id;
+
+				target.dataset.state = newState;
+
+				// Mark session as favorite
+				if (newState === 'on') {
+					target.parentNode.classList.add('is-favorite');
+				} else {
+					target.parentNode.classList.remove('is-favorite');
+				}
+
+				let headers = {
+					'X-WP-Nonce': wpApiSettings.nonce,
+				};
+
+				let savedSessions = localStorage.getItem('_sympose_saved_sessions');
+
+				if (savedSessions === null) {
+					savedSessions = [];
+				} else {
+					savedSessions = JSON.parse(savedSessions);
+				}
+
+				fetch(url, {
+						method: "POST",
+						headers: headers,
+						body: JSON.stringify({
+							'state': newState,
+							'id': sessionID,
+							'event_id': eventID,
+							'saved_sessions': savedSessions
+						})
+					})
+					.then((response) => {
+						return response.json();
+					})
+					.then((result) => {
+
+						// Always update localStorage.
+						localStorage.setItem('_sympose_saved_sessions', JSON.stringify(result.data));
+
+						if (result.status === 200) {
+							// that's fine.
+						}
+					});
+			}
+		});
+	});
+
+
+
+
+});
