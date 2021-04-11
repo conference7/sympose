eb92dd2d591b88524ccfe6d76c6819866da5351c
Marinus Klasen <marinus@mklasen.nl>
2021-04-11 17:51:02 +0000

    Remove logging

--- js/src/admin/quick-start.js
+++ js/src/admin/quick-start.js
@@ -117,7 +117,7 @@ jQuery(document).ready(function ($) {
             contentType: false,
             data: formData,
             success: function (data) {
-                console.log(data)
+                //
             }
         })
     });
