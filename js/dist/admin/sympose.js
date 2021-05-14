a95f8a0bb530e723c789773344648b4c246c6459
Marinus Klasen <marinus@mklasen.nl>
2021-05-14 15:22:59 +0000

    Add javascript handler for migration

--- js/dist/admin/sympose.js
+++ js/dist/admin/sympose.js
@@ -1,5 +1,32 @@
 "use strict";
 
+document.addEventListener('DOMContentLoaded', function () {
+  var startMigrationButton = document.querySelector('a[data-action="sympose-start-migration"]');
+
+  if (startMigrationButton !== null) {
+    startMigrationButton.addEventListener('click', function () {
+      startMigrationButton.nextElementSibling.classList.add('is-active');
+      startMigrationButton.setAttribute('disabled', true);
+      var url = wpApiSettings.root + 'sympose/v1/migrate';
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          version: startMigrationButton.dataset.version
+        })
+      }).then(function (response) {
+        return response.json();
+      }).then(function (result) {
+        if (result.status === 200) {
+          startMigrationButton.nextElementSibling.classList.remove('is-active');
+          startMigrationButton.setAttribute('disabled', false);
+          startMigrationButton.innerHTML = 'Done!';
+        }
+      });
+    });
+  }
+});
+"use strict";
+
 jQuery(document).ready(function ($) {
   refreshPage();
   var form = $('form#sympose-quick-start'); // Next step
